95a07476295d9b50fa3a09a661f5621b638603a4
Lorenzo Buonanno <l.buonanno@minddistrict.com>
2021-04-06 23:47:15 +0200

    Make avatar component more robust

--- src/Avatar.svelte
+++ src/Avatar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import type { User } from "./interfaces";
-  export let user: User;
+  export let user: User | undefined;
 </script>
 
 <div class="card">
-  <img alt="user pic" src={user.img_src} />
-  <p class="has-text-centered">{user.username}</p>
+  <img alt="user pic" src={user ? user.img_src : ""} />
+  <p class="has-text-centered">{user ? user.username : ""}</p>
 </div>
 
 <style>
